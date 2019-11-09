5008f5b572dcdc86b661a25c380655fbacf30f6f
chad187 <chad187@gmail.com>
2019-11-09 10:07:32 -0800

    currently connecting to quizizz

--- content.js
+++ content.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
   // Select the target node (tweet modal)
-  var target = $('.PermalinkOverlay-modal').get(0);
-
-  // Create an observer instance
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-
-      // Get the Twitter modal window replies count
-      var loneTweetsCount = $('.PermalinkOverlay-body .ThreadedConversation--loneTweet .tweet').length
-      var threadedTweetsCount = $('.PermalinkOverlay-body .ThreadedConversation .tweet').length
-      var total = loneTweetsCount + threadedTweetsCount
-
-      // Send message to background.js so we can set the badge text
-      chrome.extension.sendMessage({'count': total})
-
+  setTimeout(function(){
+    var target = document.getElementsByClassName("report-players").item(0)
+    var children = target.childNodes;
+    var array = Array.from(children);
+    for (item of array) { 
+      console.log(item);
+    }
+    
+    // Create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        // Get the Twitter modal window replies count
+        console.log(mutation.target.firstChild.nodeValue)
+      });
     });
-  });
 
-  // Configuration of the observer
-  var config = { attributes:true, subtree: true };
+    // Configuration of the observer
+    var config = { 
+      childList: true,
+      subtree: true,
+      attributes: true,
+      characterData: true
+    };
 
-  // Pass in the target node, as well as the observer options
-  observer.observe(target, config);
-
+    // Pass in the target node, as well as the observer options
+    observer.observe(target, config);
+  }, 10000);
 });
